1f94cd7a24055ca266e8472e8f94c37fa1344764
parnpresso <parnpresso@gmail.com>
2019-01-05 20:50:13 +0700

    Display in admin :crown:

--- pm2.5-backend/pm25/pm25/aqis/admin.py
+++ pm2.5-backend/pm25/pm25/aqis/admin.py
@@ -2,8 +2,18 @@ from django.contrib import admin
 
 from aqis.models import Aqi
 
+
 @admin.register(Aqi)
 class AqiAdmin(admin.ModelAdmin):
-    list_display = ('city', 'pm_value', 'pm_level', 'datetime')
-    list_filter = ('city', 'pm_value', 'pm_level', 'datetime')
-    search_fields = ['city', 'pm_value', 'pm_level', 'datetime']
+    list_display = (
+        'city',
+        'datetime'
+    )
+    list_filter = (
+        'city',
+        'datetime'
+    )
+    search_fields = [
+        'city',
+        'datetime'
+    ]
