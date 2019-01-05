6c69289371e6086617fa0543eccea32eeef08b18
parnpresso <parnpresso@gmail.com>
2019-01-05 20:41:15 +0700

    Add models detail for city :crown:

--- pm2.5-backend/pm25/pm25/cities/admin.py
+++ pm2.5-backend/pm25/pm25/cities/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from cities.models import City
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+    search_fields = ['name']
